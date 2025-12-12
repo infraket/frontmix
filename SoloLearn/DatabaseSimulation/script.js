85d15d886e550a097b3798cc0971ae58b61801a0
infraket <aveacvale@mail.ru>
2025-12-12 12:49:24 +0300

    db insert radio and cheked

--- SoloLearn/DatabaseSimulation/script.js
+++ SoloLearn/DatabaseSimulation/script.js
@@ -18,7 +18,39 @@ function processFormData() {
     document.getElementById('city').value = '';
     document.getElementById('country').value = '';
 };
+function insertDataReg(email, password, isSubscribed, method) {
+    let table = document.getElementById('dataTableReg');
+    let newRow = table.insertRow();
+    let emailCell = newRow.insertCell(0);
+    let passCell = newRow.insertCell(1);
+    let subscribeCell = newRow.insertCell(2);
+    let methodCell = newRow.insertCell(3);
 
+    emailCell.textContent = email;
+    passCell.textContent = password;
+    subscribeCell.textContent = isSubscribed;
+    methodCell.textContent = method;
+
+}
+function processFormDataReg() {
+    let form = document.getElementById('myFormReg');
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('pass').value;
+    let isSubscribed = document.getElementById('subscribe').checked;
+
+    let method = form.elements.pay.value;
+
+
+    //Insert data into table with predefined function
+    insertDataReg(email, password, isSubscribed, method);
+
+    // Clear the form fields
+    document.getElementById('email').value = '';
+    document.getElementById('pass').value = '';
+    document.getElementsByName('pay').value = '';
+
+
+};
 
 
 function changeStyle() {
@@ -30,6 +62,3 @@ function changeStyle() {
     body.style.color = color;
 };
 
-function testStyle() {
-    alert("Hello World");
-};
